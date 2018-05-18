77ca9388032315c66476fe9270810892732e3c05
awk11 <awk11@outlook.com>
2018-05-18 04:54:37 -0400

    Code comenting and media zoom
    -Added a feature allowing for the user to zoom in / focus on a project's
    media.
    -added commenting for a lot of the code
    -general code cleanup

--- Portfolio/Scripts/main.js
+++ Portfolio/Scripts/main.js
@@ -34,15 +34,19 @@
 })
 
 
+// Shows the project display
 function DisplayProject(projectType, projectIndex) {
+    // Sets up all of the default html for the display
     var display = document.getElementById('projectDisplay');
     display.innerHTML = "<div id='myCarousel' class='carousel slide col-sm-6' data-ride='carousel' data-interval='false'><!--Indicators--><ol id='projCarouselInds' class='carousel-indicators'><li data-target='#myCarousel' data-slide-to='0' class=active'></li></ol><!--Wrapper for slides--><div id='projMedia' class='carousel-inner text-center' role='listbox'><div class='item active'></div></div><!--Left and right controls--><a class='left carousel-control' href='#myCarousel' role='button' data-slide='prev'><span class='glyphicon glyphicon-chevron-left' aria-hidden='true'></span><span class='sr-only'>Previous</span></a><a class='right carousel-control' href='#myCarousel' role='button' data-slide='next'><span class='glyphicon glyphicon-chevron-right' aria-hidden='true'></span><span class='sr-only'>Next</span></a></div ><div id='projDetails' class='col-sm-6 slide'></div>";
     display.style.display = 'block';
 
+    // Locks scrolling for the page and shows the fade
     document.body.scroll = "no";
     document.body.style.overflow = "hidden";
     document.getElementById("fade").style.display = "block";
 
+    // Calls a back end function to grab the appropriate project data
     $.ajax({
         cache: false,
         type: "POST",
@@ -53,14 +57,16 @@ function DisplayProject(projectType, projectIndex) {
         },
         datatype: "json",
         success: function (result) {
+            // parses the json returned into an array of strings
             var projArray = JSON.parse(result);
 
+            // Goes through all of the media data and sets up the carousel
             var projMedia = document.getElementById('projMedia');
             var displayInnerHTML = '';
             for (var i = 2; i < projArray.length; i++) {
                 displayInnerHTML += '<div class="item ';
                 if (i == 2) displayInnerHTML += 'active';
-                displayInnerHTML += '">';
+                displayInnerHTML += '" onclick="MediaZoom();">';
                 if (projArray[i].includes('mp4')) {
                     displayInnerHTML += '<video controls><source src="' + projArray[i] + '" type="video/mp4">Your browser does not support the video tag. Sorry about that! Please try again in a more modern browser.</video></div>';
                 }
@@ -73,6 +79,7 @@ function DisplayProject(projectType, projectIndex) {
             }
             projMedia.innerHTML = displayInnerHTML;
 
+            // Sets up the indicator bar for the carousel
             var indicators = document.getElementById('projCarouselInds');
             displayInnerHTML = '';
             for (var i = 0; i < projArray.length - 2; i++) {
@@ -82,6 +89,7 @@ function DisplayProject(projectType, projectIndex) {
             }
             indicators.innerHTML = displayInnerHTML;
 
+            // Adds the description for the project to the view
             displayInnerHTML = '<h4>' + projArray[0] + '</h4><p>' + projArray[1] + '</p>';
             document.getElementById('projDetails').innerHTML = displayInnerHTML;
         },
@@ -91,6 +99,7 @@ function DisplayProject(projectType, projectIndex) {
     });
 }
 
+// Closes the project view display
 function CloseProject(e) {
     if (e.target.id !== 'fade' && e.target.id !== 'projectDisplay') return;
     document.body.scroll = "yes";
@@ -99,4 +108,40 @@ function CloseProject(e) {
     document.getElementById("projectDisplay").style.display = "none";
     document.getElementById("projectDisplay").innerHTML = "";
     document.getElementById("fade").style.display = "none";
+}
+
+// Zooms in or out of the project media
+function MediaZoom() {
+    var projDetails = document.getElementById('projDetails');
+    // If not zoomed in already
+    if (projDetails.style.display != "none") {
+        // Sets up all the necessary css to allow for the media images to be focused on
+        projDetails.style.display = "none";
+        var items = document.getElementsByClassName('item');
+        for (var i = 0; i < items.length; i++) {
+            items[i].firstElementChild.style.maxWidth = "1000px";
+            items[i].firstElementChild.style.maxHeight = "900px";
+        }
+        var projMedia = document.getElementById('myCarousel');
+        projMedia.style.display = "inline-block";
+        projMedia.style.position = "relative";
+        projMedia.style.width = "100%";
+        projMedia.style.textAlign = "center";
+        projMedia.style.cssFloat = "none";
+    }
+    else {  // If already zoomed in
+        // Clears all of the custom set css properties from when the user zoomed in
+        projDetails.style.display = "";
+        var items = document.getElementsByClassName('item');
+        for (var i = 0; i < items.length; i++) {
+            items[i].firstElementChild.style.maxWidth = "";
+            items[i].firstElementChild.style.maxHeight = "";
+        }
+        var projMedia = document.getElementById('myCarousel');
+        projMedia.style.display = "";
+        projMedia.style.position = "";
+        projMedia.style.width = "";
+        projMedia.style.textAlign = "";
+        projMedia.style.cssFloat = "";
+    }
 }
